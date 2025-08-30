0518fa465f09bd78f4a044d5db2c81fb79ac02b7
Bhavesh Varu <bhavesh.varu97@gmail.com>
2025-08-30 21:52:56 +0530

    added menu and chat component

---
+++ src/components/chatList/ChatList.jsx
@@ -0,0 +1,32 @@
+import { Link } from 'react-router-dom'
+import './chatList.css'
+
+const ChatList = () => {
+  return (
+    <div className='chatList'>
+      <span className='title'>DashBoard</span>
+      <Link to='/dashboard'>Create a New Chat </Link>
+      <Link to='/'>Explore PromptLab</Link>
+      <Link to='/'>My Chat Title</Link>
+      <hr />
+      <span className='title'>Recent Chats</span>
+      <div className='list'>
+        <Link to='/'>My Chat Title</Link>
+        <Link to='/'>My Chat Title</Link>
+        <Link to='/'>My Chat Title</Link>
+        <Link to='/'>My Chat Title</Link>
+        <Link to='/'>My Chat Title</Link>
+      </div>
+      <hr />
+      <div className='upgrade'>
+        <img src='/logo.png' alt='logo' />
+        <div className='texts'>
+          <span>Upgrade to Prompt AI Pro</span>
+          <span>Get unlimited access to all features</span>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ChatList
--- src/layouts/dashboardLayout/DashboardLayout.jsx
+++ src/layouts/dashboardLayout/DashboardLayout.jsx
@@ -2,6 +2,7 @@ import { Outlet, useNavigate } from 'react-router-dom'
 import './dashboardLayout.css'
 import { useAuth } from '@clerk/clerk-react'
 import { useEffect } from 'react'
+import ChatList from '../../components/chatList/ChatList'
 
 const DashboardLayout = () => {
   const { userId, isLoaded } = useAuth()
@@ -20,9 +21,10 @@ const DashboardLayout = () => {
   return (
     <div className='dashboardLayout'>
       <div className='menu'>
-        <div className='content'>
-          <Outlet />
-        </div>
+        <ChatList />
+      </div>
+      <div className='content'>
+        <Outlet />
       </div>
     </div>
   )
